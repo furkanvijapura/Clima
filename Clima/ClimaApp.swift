daf077dbec2743f3159723ad4183692267247040
MdMugish <mohammad.mugish@gmail.com>
2021-01-25 21:38:51 +0530

    Initial Commit

---
+++ Clima/ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaApp.swift
+//  Clima
+//
+//  Created by mohammad mugish on 25/01/21.
+//
+
+import SwiftUI
+
+@main
+struct ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
